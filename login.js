86b93d76180a4e9207a05bf16ddd0ecefe18cb25
Abbie Barton <abbie.s.barton@gmail.com>
2023-10-28 11:44:09 -0600

    fixing login - new page & functionality

--- login.js
+++ login.js
@@ -1,33 +1,12 @@
 const login = () => {
-  const nameElement = document.querySelector("#username");
-  const passwordElement = document.querySelector("#password");
-  const modal = document.querySelector("#modal");
-  const accountText = document.getElementById("user");
-  console.log("name: ", nameElement.value, " password: ", passwordElement.value)
+
+  const nameElement = document.getElementById("username");
+  const passwordElement = document.getElementById("password");
+
   if (nameElement.value && passwordElement.value) {
     localStorage.setItem("userName", nameElement.value);
     localStorage.setItem("password", passwordElement.value);
-    // make the modal go away, show 'my account' once logged in
-    modal.style.display = "none";
-    // accountText.innerHTML = "my account";
+
+    window.location.href = "home.html";
   } 
-};
-
-const showModal = () => {
-  const modal = document.querySelector("#modal");
-  const openModal = document.querySelector("#user");
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-
-  if (localStorage.getItem("userName") === null) {
-    modal.style.display = "block";
-  } else {
-    window.location.href =
-      "https://startup.makeitgood.click/pages/account.html";
-  }
 };
