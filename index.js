77cacc944b6a068e5a2e0beba2bc12560e45ac55
Abbie Barton <abbie.s.barton@gmail.com>
2023-11-22 16:33:26 -0700

    linking logout/signup to html

--- index.js
+++ index.js
@@ -21,23 +21,8 @@ app.use(express.static('public'));
 // Trust headers that are forwarded from the proxy so we can determine IP addresses
 app.set('trust proxy', true);
 
-// Router for service endpoints
-const apiRouter = express.Router();
+var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
-
-// secureApiRouter verifies credentials for endpoints
-var secureApiRouter = express.Router();
-apiRouter.use(secureApiRouter);
-
-secureApiRouter.use(async (req, res, next) => {
-  authToken = req.cookies[authCookieName];
-  const user = await db.getUserByToken(authToken);
-  if (user) {
-    next();
-  } else {
-    res.status(401).send({ msg: 'Unauthorized' });
-  }
-});
 
 //auth&cookie endpoints
 // setAuthCookie in the HTTP response
@@ -51,10 +36,12 @@ const setAuthCookie = (res, authToken) => {
 
 // CreateAuth token for a new user
 apiRouter.post('/auth/create', async (req, res) => {
-  if (await db.getUser(req.body.email)) {
+  console.log("inside create auth token for new user /auth/create")
+  if (await db.getUser(req.body.userName)) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = await db.createUser(req.body.email, req.body.password);
+    const user = await db.createUser(req.body.userName, req.body.password);
+    console.log("inside create user, req.body.password: " + req.body.password)
 
     // Set the cookie
     setAuthCookie(res, user.token);
@@ -67,7 +54,8 @@ apiRouter.post('/auth/create', async (req, res) => {
 
 // GetAuth token for the provided credentials
 apiRouter.post('/auth/login', async (req, res) => {
-  const user = await db.getUser(req.body.email);
+  console.log("inside /auth/login getauth token")
+  const user = await db.getUser(req.body.userName);
   if (user) {
     if (await bcrypt.compare(req.body.password, user.password)) {
       setAuthCookie(res, user.token);
@@ -85,14 +73,29 @@ apiRouter.delete('/auth/logout', (_req, res) => {
 });
 
 // GetUser returns information about a user
-apiRouter.get('/user/:email', async (req, res) => {
-  const user = await db.getUser(req.params.email);
+apiRouter.get('/user/:userName', async (req, res) => {
+  console.log("inside login method /user/:userName")
+  const user = await db.getUser(req.params.userName);
   if (user) {
     const token = req?.cookies.token;
-    res.send({ email: user.email, authenticated: token === user.token });
+    res.send({ userName: user.userName, authenticated: token === user.token });
     return;
   }
   res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await db.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
 });
 
 // service endpoints
--- public/login.js
+++ public/login.js
@@ -1,12 +1,61 @@
 const login = () => {
-
   const nameElement = document.getElementById("username");
   const passwordElement = document.getElementById("password");
-
   if (nameElement.value && passwordElement.value) {
-    localStorage.setItem("userName", nameElement.value);
-    localStorage.setItem("password", passwordElement.value);
-
-    window.location.href = "home.html";
-  } 
+    loginUser();
+  }
 };
+
+const signup = () => {
+  const nameElement = document.getElementById("username");
+  const passwordElement = document.getElementById("password");
+  if (nameElement.value && passwordElement.value) {
+    createUser();
+  }
+}
+
+(async () => {
+  const userName = localStorage.getItem("userName");
+  if (userName) {
+    window.location.href = "home.html";
+  }
+})();
+
+const loginUser = async () => {
+  loginOrCreate(`/api/auth/login`);
+}
+
+const createUser = async () => {
+  loginOrCreate(`/api/auth/create`);
+}
+
+const loginOrCreate = async (endpoint) => {
+  const userName = document.getElementById("username")?.value;
+  const password = document.getElementById("password")?.value;
+  const response = await fetch(endpoint, {
+    method: "post",
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  console.log(response);
+
+  if (response.ok) {
+    localStorage.setItem("userName", userName);
+    window.location.href = "home.html";
+  } else {
+    const body = await response.json();
+    alert(`⚠ Error: ${body.msg}`)
+  }
+}
+
+const getUser = async (email) => {
+  // See if we have a user with the given email.
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
+}
