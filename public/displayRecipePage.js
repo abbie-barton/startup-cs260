4ddf7c5b06b27e025dce71b936e3cb6a0399afaf
Abbie Barton <abbie.s.barton@gmail.com>
2023-11-30 18:36:05 -0700

    move simon code & examples to displayRecipePage.js

--- public/displayRecipePage.js
+++ public/displayRecipePage.js
@@ -79,27 +79,6 @@ const addNewComment = () => {
   location.reload();
 };
 
-const favoriteRecipe = async () => {
-  const user = await getUser(localStorage.getItem("userName"));
-  const recipe = JSON.parse(localStorage.getItem('recipe'));
-  saveFavorite(user.userName, recipe.recipe);
-  alert('recipe favorited successfully');
-};
-
-const saveFavorite = async (userName, recipe) => {
-  try {
-    const response = await fetch(`/api/favorited-recipe`, {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ userName, recipe }),
-    });
-    const user = await response.json();
-    return user;
-  } catch {
-    console.error('error saving favorite recipe /favorited-recipe');
-  }
-}
-
 const saveComment = async (name, commentText, id) => {
   try {
     const response = await fetch(`/api/comment?id=${id}`, {
@@ -141,3 +120,116 @@ const getUser = async (userName) => {
 
 // id stored in local storage for current recipe page
 getRecipe(localStorage.getItem("id"));
+
+// favorite recipe javascript & websocket broadcast
+// javascript
+const favoriteRecipe = async () => {
+  const user = await getUser(localStorage.getItem("userName"));
+  const recipe = JSON.parse(localStorage.getItem('recipe'));
+  saveFavorite(user.userName, recipe.recipe);
+  alert('recipe favorited successfully');
+};
+
+const saveFavorite = async (userName, recipe) => {
+  try {
+    const response = await fetch(`/api/favorited-recipe`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ userName, recipe }),
+    });
+    const user = await response.json();
+    return user;
+  } catch {
+    console.error('error saving favorite recipe /favorited-recipe');
+  }
+}
+
+//websocket
+class Game {
+  socket;
+
+  constructor() {
+    this.configureWebSocket();
+  }
+
+  async reset() {
+    this.allowPlayer = false;
+    this.playerPlaybackPos = 0;
+    this.sequence = [];
+    this.updateScore('--');
+    await this.buttonDance(1);
+    this.addButton();
+    await this.playSequence();
+    this.allowPlayer = true;
+
+    // Let other players know a new game has started
+    this.broadcastEvent(this.getUserName(), GameStartEvent, {});
+  }
+
+  getUserName() {
+    return localStorage.getItem('userName') ?? 'Mystery player';
+  }
+
+  async saveScore(score) {
+    const userName = this.getPlayerName();
+    const date = new Date().toLocaleDateString();
+    const newScore = { name: userName, score: score, date: date };
+
+    try {
+      const response = await fetch('/api/score', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newScore),
+      });
+
+      // Let other players know the game has concluded
+      this.broadcastEvent(userName, GameEndEvent, newScore);
+
+      // Store what the service gave us as the high scores
+      const scores = await response.json();
+      localStorage.setItem('scores', JSON.stringify(scores));
+    } catch {
+      // If there was an error then just track scores locally
+      this.updateScoresLocal(newScore);
+    }
+  }
+
+  // Functionality for peer communication using WebSocket
+
+  configureWebSocket() {
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    this.socket.onopen = (event) => {
+      this.displayMsg('system', 'game', 'connected');
+    };
+    this.socket.onclose = (event) => {
+      this.displayMsg('system', 'game', 'disconnected');
+    };
+    this.socket.onmessage = async (event) => {
+      const msg = JSON.parse(await event.data.text());
+      if (msg.type === GameEndEvent) {
+        this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+      } else if (msg.type === GameStartEvent) {
+        this.displayMsg('player', msg.from, `started a new game`);
+      }
+    };
+  }
+
+  displayMsg(cls, from, msg) {
+    const chatText = document.querySelector('#player-messages');
+    chatText.innerHTML =
+      `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+  }
+
+  broadcastEvent(from, type, value) {
+    const event = {
+      from: from,
+      type: type,
+      value: value,
+    };
+    this.socket.send(JSON.stringify(event));
+  }
+}
+
+const game = new Game();
+
