979d4f80d89a05ba06ab5e23ff27573f8eb2f73e
Abbie Barton <abbie.s.barton@gmail.com>
2023-12-08 11:11:41 -0700

    install bootstrap & tailwind, add src folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div>
+      <p>App will display here</p>
+      <Header />
+    </div>
+  );
+}
