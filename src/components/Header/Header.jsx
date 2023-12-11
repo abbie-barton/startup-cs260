e7959bda09295fcc46ca0606c56c755fc97d5d3e
Abbie Barton <abbie.s.barton@gmail.com>
2023-12-11 01:55:26 -0700

    conditionally render Header & login page functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,49 @@
 import React from "react";
-import './header.css';
-import { NavLink } from "react-router-dom";
+import "./header.css";
+import { NavLink, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const location = useLocation();
+  const [isLoginPage, setIsLoginPage] = useState(false);
+
+  useEffect(() => {
+    const path = location.pathname;
+    if (path === "/" || path === "") {
+      setIsLoginPage(true);
+    }
+  }, [location]);
+
   return (
     <>
       <header>
-      <div className="flex flex-row">
-        <img id="logo" src="/assets/startupLogo.svg" alt="make it good logo" />
-        <h1 className="text-4xl font-normal self-center">Make it Good</h1>
-      </div>
-      <ul>
-        <li><NavLink to="home">home</NavLink></li>
-        <li><NavLink to="recipe-catalog">recipe catalog</NavLink></li>
-        <li><NavLink to="upload-recipe">upload recipe</NavLink></li>
-        <li><NavLink to="account">my account</NavLink></li>
-      </ul>
-    </header>
+        <div className="flex flex-row">
+          <img
+            id="logo"
+            src="/assets/startupLogo.svg"
+            alt="make it good logo"
+          />
+          <h1 className="text-4xl font-normal self-center">Make it Good</h1>
+        </div>
+        {isLoginPage ? (
+          ""
+        ) : (
+          <ul>
+            <li>
+              <NavLink to="home">home</NavLink>
+            </li>
+            <li>
+              <NavLink to="recipe-catalog">recipe catalog</NavLink>
+            </li>
+            <li>
+              <NavLink to="upload-recipe">upload recipe</NavLink>
+            </li>
+            <li>
+              <NavLink to="account">my account</NavLink>
+            </li>
+          </ul>
+        )}
+      </header>
     </>
   );
 }
--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,11 +1,108 @@
-import React from 'react';
-import Header from '../../components/Header/Header'
+import React from "react";
+import Header from "../../components/Header/Header";
+import '../../globalStyles.css';
+import { useState } from "react";
 
 export default function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    if (userName && password) {
+      loginUser();
+    }
+  };
+
+  const signup = () => {
+    if (userName && password) {
+      createUser();
+    }
+  };
+
+  (async () => {
+    const userName = localStorage.getItem("userName");
+    if (userName) {
+      window.location.href = "home.html";
+    }
+  })();
+
+  const loginUser = async () => {
+    loginOrCreate(`/api/auth/login`);
+  };
+
+  const createUser = async () => {
+    loginOrCreate(`/api/auth/create`);
+  };
+
+  const loginOrCreate = async (endpoint) => {
+    const response = await fetch(endpoint, {
+      method: "post",
+      body: JSON.stringify({ userName: userName, password: password }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    console.log(response);
+
+    if (response.ok) {
+      localStorage.setItem("userName", userName);
+      window.location.href = "home.html";
+    } else {
+      const body = await response.json();
+      alert(`⚠ Error: ${body.msg}`);
+    }
+  };
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
+    <main className="container-fluid  text-center">
       <Header />
-      <div>login displayed here</div>
+      <div id="login">
+        <h1>Log in</h1>
+        <form id="login-form" className="d-flex flex-column">
+          <label htmlFor="username" className="form-label">
+            Username:
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="form-control"
+            required
+            autoComplete="on"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="form-control"
+            required
+            autoComplete="on"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="d-flex flex-row justify-content-center pt-3">
+            <button
+              type="reset"
+              className="btn btn-outline-dark pt-2 mx-1"
+              id="login-button"
+              onClick={login}
+            >
+              Log in
+            </button>
+            <button
+              type="reset"
+              className="btn btn-outline-dark pt-2 mx-1"
+              id="signup-button"
+              onClick={signup}
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
