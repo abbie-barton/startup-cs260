8ec9e2a8f168b5c592b2e030e646d64380267404
Abbie Barton <abbie.s.barton@gmail.com>
2023-12-12 21:33:20 -0700

    recipe catalog

--- src/containers/recipeCatalog/RecipeCatalog.jsx
+++ src/containers/recipeCatalog/RecipeCatalog.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import RecipeCard from "../../components/RecipeCard/RecipeCard";
 
 export default function RecipeCatalog() {
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecentRecipes = async () => {
+    try {
+      const response = await fetch("/api/recent-recipes");
+      const data = await response.json();
+      setRecipes(data);
+    } catch {
+      console.error("failed to fetch /all-recipes");
+    }
+  };
+
+  useEffect(() => {
+    getRecentRecipes();
+  }, [])
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
-    </main>
+    <>
+      <Header />
+      <div className="container text-start py-4">
+        <div className="row g-2">
+          <div id="recent-recipes" className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+            {recipes.map((recipe, index) => (
+              <RecipeCard key={index} recipe={recipe.recipe} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
